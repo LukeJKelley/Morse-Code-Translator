aacf1b33b55c495a37896fc2fde257b88d82c075
Luke Kelley <lj_kelley@hotmail.com>
2022-11-30 15:34:06 +1100

    finished project -no bonus

--- javascript/script.js
+++ javascript/script.js
@@ -1,49 +1,15 @@
-const code = {
-  A: ".-",
-  B: "-...",
-  C: "-.-.",
-  D: "-..",
-  E: ".",
-  F: "..-.",
-  G: "--.",
-  H: "....",
-  I: "..",
-  J: ".---",
-  K: "-.-",
-  L: ".-..",
-  M: "--",
-  N: "-.",
-  O: "---",
-  P: ".--.",
-  Q: "--.-",
-  R: ".-.",
-  S: "...",
-  T: "-",
-  U: "..-",
-  W: ".--",
-  X: "-..-",
-  Y: "-.--",
-  Z: "--..",
-};
+import { code, convertToMorse } from "../modules/modules.js";
 
 // English text area
 const engInput = document.querySelector("#englishTranslation");
-console.log(engInput);
 
 //Morse Output area
 const morseOutput = document.querySelector("#morseTranslation");
-console.log(morseOutput.value);
-// Fix above line
 
 // Submit button
 const submitBtn = document.querySelector("#submitBtn");
 console.log(submitBtn);
 
-// submitBtn.addEventListener("click", () => {
-//   engInput.value = "translated";
-//   console.log(morseOutput.value);
-// });
-
-morseOutput.addEventListener("input", () => {
-  engInput.value = morseOutput.value;
+engInput.addEventListener("input", () => {
+  morseOutput.value = convertToMorse(engInput.value);
 });
